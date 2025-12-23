429945f7ac90d136434ac458b3dc3ec5c120e7a4
Sean Colsen <colsen.sean@gmail.com>
2025-12-22 22:16:11 -0500

    Fix some misc files with prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,14 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -20,22 +20,22 @@ export default defineConfig([
       globals: globals.browser,
     },
     rules: {
-      '@typescript-eslint/naming-convention': [
-        'error',
+      "@typescript-eslint/naming-convention": [
+        "error",
         {
-          selector: 'variableLike',
-          format: ['snake_case', 'UPPER_CASE'],
-          leadingUnderscore: 'allow',
+          selector: "variableLike",
+          format: ["snake_case", "UPPER_CASE"],
+          leadingUnderscore: "allow",
         },
         {
-          selector: 'typeLike',
-          format: ['PascalCase'],
+          selector: "typeLike",
+          format: ["PascalCase"],
         },
         {
-          selector: 'function',
-          format: ['snake_case', 'PascalCase'],
+          selector: "function",
+          format: ["snake_case", "PascalCase"],
         },
       ],
     },
   },
-])
+]);
